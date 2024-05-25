f63fb8bb4ef9fd6c80bdf324e1153044844bf5e3
PrasunKamilya <prasunkamilya26@gmail.com>
2024-05-25 12:38:44 +0530

    "Added Footer and HowItWorks components to App.jsx"

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import Features from "./components/Features"
+import Footer from "./components/Footer"
 import Hero from "./components/Hero"
 import Highlights from "./components/Highlights"
+import HowItWorks from "./components/HowItWorks"
 import Model from "./components/Model"
 import Navbar from "./components/Navbar"
 import * as Sentry from '@sentry/react'
@@ -14,6 +16,8 @@ function App() {
       <Highlights/>
       <Model/>
       <Features/>
+      <HowItWorks/>
+      <Footer/>
     </main>
   )
 }
